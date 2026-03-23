d40ebf1683c69cdac92b5fdc66674f3ac0ae26c1
Nikita <nikitakatrich.kzn@gmail.com>
2026-03-23 19:35:15 +0300

    made js file with translations of english and russian, type of translation is: i18n

---
+++ assets/scripts/translations.js
@@ -0,0 +1,109 @@
+export const translations = {
+  en: {
+    // h1
+    h1FirstPart: "Hello! ",
+    h1SecondPart: " My name is Nikita Katrich, I am — ",
+    h1ThirdPart: "Front-End developer.",
+    h1FourthPart: " I create user-friendly and fast interfaces for your ideas.",
+
+    // titles nav site
+    projects: "Projects",
+    skills: "Skills",
+    certificates: "Certificates",
+
+    // projs decs
+    decs1: "",
+    decs2: "Weather web-app",
+    decs3: "Online furniture shop",
+    decs4: "Interactive calculator",
+    decs5: "Discipline and motivation",
+
+    // stack
+    stack: "Stack",
+
+    // btn-side
+    btnSite: 'Web-site',
+
+    // sub-titles of skills
+    subStack: "Stack",
+    subPractice: "Front-End Practice",
+    subTools: "Tools",
+
+    // skills decs
+    stack1: "HTML5 (semantics, basic accessibility)",
+    stack2: "CSS3 (flexbox, grid, responsiveness, animations)",
+
+    // Front-End Practice skills
+    practice1: "Algorithms (Shunting Yard, Quick Sort, etc.)",
+    practice2: "Event delegation",
+    practice3: "Form validation",
+    practice4: "State management",
+    practice5: "Component-oriented architecture",
+    practice6: "localStorage manipulations",
+    practice7: "JS animations(RAF)",
+    practice8: "Performance optimisation",
+    practice9: "Routing",
+    practice10: "Working with third-party APIs",
+
+    // decs about tg
+    decsTg: "I also have my own Telegram channel where I share my experiences with projects, what I’m learning, and how it all works. By checking out the channel, you’ll open up new opportunities for yourself.",
+
+    // block of contact
+
+    infoContact: "Interested, or do you have any questions? Please get in touch:",
+    phone: "Tel: "
+  },
+  ru: {
+    h1FirstPart: "Привет! ",
+    h1SecondPart: " Меня зовут Никита Катрич, Я — ",
+    h1ThirdPart: "Front-End разработчик.",
+    h1FourthPart: " Я создаю удобные и быстрые интерфейсы для ваших идей.",
+
+    // titles nav site
+    projects: "Проекты",
+    skills: "Навыки",
+    certificates: "Сертификаты",
+
+    // projs decs
+    decs1: "",
+    decs2: "Веб-приложение погода",
+    decs3: "Интернет магазин мебели",
+    decs4: "Интерактивный калькулятор",
+    decs5: "Дисциплина и мотивация",
+
+    // stack
+    stack: "Стек",
+
+    // btn-side
+    btnSite: 'Сайт',
+
+    // sub-titles of skills
+    subStack: "Стек",
+    subPractice: "Front-End Практика",
+    subTools: "Инструменты",
+
+    // skills decs
+    stack1: "HTML5(Семантика, базовая доступность)",
+    stack2: "CSS3(flexbox, grid, адаптивность, анимации)",
+
+    // Front-End Practice skills
+    practice1: "Алгоритмы(Shunting Yard, Quick Sort и т. п.)",
+    practice2: "Делегирование событий",
+    practice3: "Валидация форм",
+    practice4: "Обработка состояний",
+    practice5: "Компонентно-ориентированная-архитектура",
+    practice6: "localStorage манипуляции",
+    practice7: "JS-анимации(RAF)",
+    practice8: "Оптимизация выполнения",
+    practice9: "Маршрутизация",
+    practice10: "Работа со сторонними API",
+
+    // decs about tg
+    decsTg: "Так же, у меня есть собственный Telegram канал, где я рассказываю свой опыт в проектах, что изучаю, рассказываю как это работает. Посмотрев канал, вы откроете для себя новые двери.",
+
+    // block of contact
+
+    infoContact: "Заинтересовались или есть какие либо вопросы? Пишите:",
+    phone: "Телефон: "
+  }
+};
